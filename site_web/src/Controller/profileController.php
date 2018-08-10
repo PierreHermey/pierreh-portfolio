f2e1ddb07ddda64a3e21ab87b9491a6fdc37ad73
PierreHermey <pierre.hermey@laposte.net>
2018-08-10 15:59:58 +0200

    adding some things

--- site_web/src/Controller/profileController.php
+++ site_web/src/Controller/profileController.php
@@ -5,7 +5,7 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
+use App\Entity\Experiences;
 
 class profileController extends Controller
 {
@@ -16,9 +16,12 @@ class profileController extends Controller
     public function profile()
     {
         $profile = 'Profile';
+        
+        
 
         return $this->render('partials/profile.html.twig', array(
             'Profile' => $profile,
+            'jobs' => $jobs
         ));
     }
 }
